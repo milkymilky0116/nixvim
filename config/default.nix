ac791273752ba4672c336f17a00f7bde71aaed68
milkymilky0116 <sjlee990129@gmail.com>
2024-07-16 13:43:51 +0900

    feat: colorscheme

---
+++ config/default.nix
@@ -0,0 +1,21 @@
+{ config
+, lib
+, ...
+}: {
+  imports = [
+    ./plug/colorscheme/colorscheme.nix
+];
+  options = {
+    theme = lib.mkOption {
+      default = lib.mkDefault "tokyonight";
+      type = lib.types.enum [ "tokyonight" "decay" "edge-dark" "mountain" "tokyonight" "everforest" "everblush" "jellybeans" "aquarium" "gruvbox" ];
+    };
+  };
+  config = {
+    # The base16 theme to use, if you want to use another theme, change it in colorscheme.nix
+    theme = "tokyonight";
+    extraConfigLua = ''
+      _G.theme = "${config.theme}"
+    '';
+  };
+}
