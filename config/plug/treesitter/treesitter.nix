6f43a8a5ccb9fec2678dffee8473847d6c01d71e
milkymilky0116 <sjlee990129@gmail.com>
2024-07-16 14:32:02 +0900

    feat: treesitter

--- config/plug/treesitter/treesitter.nix
+++ config/plug/treesitter/treesitter.nix
@@ -1,7 +1,6 @@
 {pkgs, ... }: {
     plugins.treesitter = {
         enable = true;
-        indent.enable = true;
         folding = false;
         nixvimInjections = true;
         grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
