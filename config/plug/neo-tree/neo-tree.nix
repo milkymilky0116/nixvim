49d0b58bc1287b56f495566064f9b3ce77708f00
milkymilky0116 <sjlee990129@gmail.com>
2024-07-16 16:29:19 +0900

    fix: neo-tree

--- config/plug/neo-tree/neo-tree.nix
+++ config/plug/neo-tree/neo-tree.nix
@@ -3,5 +3,10 @@
         enable = true;
         enableGitStatus = true;
         enableModifiedMarkers = true;
+        filesystem = {
+          filtered_items = {
+            hide_gitignore = false;
+          };
+        };
     };
 }
