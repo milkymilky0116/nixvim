71a64feec5db5b2bc668636005f54d3041f0c2e8
milkymilky0116 <sjlee990129@gmail.com>
2024-07-16 15:19:17 +0900

    feat: vim default setting

--- config/sets.nix
+++ config/sets.nix
@@ -4,6 +4,24 @@
       # Enable relative line numbers
       number = true;
       relativenumber = true;
+      tabstop = 2;
+      softtabstop = 2;
+      showtabline = 2;
+      expandtab = true;
+      # Enable auto indenting and set it to spaces
+      smartindent = true;
+      shiftwidth = 2;
+
+      # Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
+      breakindent = true;
+      wrap = true;
+      termguicolors = true;
+      cursorline = true;
+      scrolloff = 8;
+      encoding = "utf-8";
+      fileencoding = "utf-8";
+      cmdheight = 0;
+      showmode = false;
     };
   };
 }
